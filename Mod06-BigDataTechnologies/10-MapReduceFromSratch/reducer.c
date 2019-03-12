931c0629b0a4e27addafb86681d09748b929c162
Akshay Terode <terodeakshay@gmail.com>
2019-03-12 09:38:11 +0530

    Update and rename mapreduce.h to reducer.c

---
+++ Mod06-BigDataTechnologies/10-MapReduceFromSratch/reducer.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define BUFFER_SIZE     1024  
+#define DELIM   "\t"
+int main(int argc, char *argv[]){
+char strLastKey[BUFFER_SIZE];
+ char strLine[BUFFER_SIZE];
+ int count = 0;
+ *strLastKey = '\0';
+ *strLine = '\0';
+ while( fgets(strLine, BUFFER_SIZE - 1, stdin) ){
+ char *strCurrKey = NULL;
+ char *strCurrNum = NULL;
+ strCurrKey  = strtok(strLine, DELIM);
+ strCurrNum = strtok(NULL, DELIM); 
+ if( strLastKey[0] == '\0'){
+  strcpy(strLastKey, strCurrKey);
+ }
+ if(strcmp(strCurrKey, strLastKey)) {
+ printf("%s\t%d\n", strLastKey, count);
+ count = atoi(strCurrNum);
+ } else {
+ count += atoi(strCurrNum);
+}
+ strcpy(strLastKey, strCurrKey);
+ }
+ printf("%s\t%d\n", strLastKey, count); 
+ return 0;
+}
